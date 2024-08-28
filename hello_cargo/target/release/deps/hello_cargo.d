d504bf935a6b2c39557a9c43b8e1db215d791eba
Paulo Viana <paulo9mv@yahoo.com.br>
2024-08-28 19:04:16 -0400

    refactor: remove auto generated files

--- hello_cargo/target/release/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-C:\Users\paulo\Documents\rust\hello_cargo\target\release\deps\hello_cargo.exe: src/main.rs
-
-C:\Users\paulo\Documents\rust\hello_cargo\target\release\deps\hello_cargo.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/release/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-C:\Users\paulo\Documents\rust\hello_cargo\target\release\hello_cargo.exe: C:\Users\paulo\Documents\rust\hello_cargo\src\main.rs
